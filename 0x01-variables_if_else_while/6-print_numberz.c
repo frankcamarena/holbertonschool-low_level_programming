a492b9712886b2e12c4156e06aa065b3c79280e6
frankcamarena <fgustavocamarena@gmail.com>
2021-10-01 23:03:24 +0000

    prints all single digit numbers of base 10 starting from 0 with var int

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - starting point
+*
+* Return: 0 if correct
+*/
+
+/* Program about prints Numbers 0-9 with var int*/
+	int main(void)
+{
+	int n;
+
+	n = 48;
+	/* your code goes there */
+	while (n <= 57)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar(10);
+	return (0);
+}
