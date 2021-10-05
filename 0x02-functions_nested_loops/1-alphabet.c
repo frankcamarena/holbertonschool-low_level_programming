dbef1bd4cde1b1ccd658df2b8a87ec891b96dc2d
frankcamarena <fgustavocamarena@gmail.com>
2021-10-05 23:11:26 +0000

    File C print alphabet wiht header File

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* print_alphabet - show letter the alphabet
+*
+* Return: 0 if correct
+*/
+
+void print_alphabet(void)
+{
+	int a = 97;
+
+	while (a <= 122)
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar(10);
+}
