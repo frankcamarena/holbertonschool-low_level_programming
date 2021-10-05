6ef48f3aacc361434e15c7650f5be179f5738f1a
frankcamarena <fgustavocamarena@gmail.com>
2021-10-05 23:54:19 +0000

    Program C show alphabetx10 whit Header Files

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - show letter the alphabet 10 times
+*
+* Return: 0 if correct
+*/
+
+void print_alphabet_x10(void)
+{
+	int a = 97;
+	int b = 0;
+
+	while (b <= 10)
+	{
+		while (a <= 122)
+		{
+			_putchar(a);
+			a++;
+		}
+		_putchar(10);
+		b++;
+		a = 97;
+
+	}
+}
