1242a4a2494846bc99e1bf6d3611d3751447e1f5
frankcamarena <fgustavocamarena@gmail.com>
2021-10-06 03:44:28 +0000

    Pogram C whit header Files

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* print_last_digit - function about to print the last digit the  of a number
+* @x: is the last digit
+* Return: x for las digit
+*/
+int print_last_digit(int x)
+
+{
+	if (x < 0)
+	{
+	x = (x * -1);
+	}
+	x = x % 10;
+	_putchar(x + '0');
+	return (x);
+}
