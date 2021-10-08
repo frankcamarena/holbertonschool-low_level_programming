63afd982900e92b99f387d4379f08f69d91ad1d8
frankcamarena <fgustavocamarena@gmail.com>
2021-10-08 03:40:46 +0000

    function that prints the numbers, from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* print_numbers - function about ckeck for a digit
+* @c: is a number
+* Return: 10 is correct
+*/
+
+void print_numbers(void)
+
+{
+	int c = 48;
+
+	while (c <= 57)
+	{
+	_putchar(c);
+	c++;
+	}
+	_putchar(10);
+}
