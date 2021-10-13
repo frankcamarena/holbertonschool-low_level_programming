b7fb7e99d98d312f959a89a86d16cd2f1b80ee71
frankcamarena <fgustavocamarena@gmail.com>
2021-10-13 03:54:05 +0000

    unction that prints every other character of a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* puts2 - function about to print str
+* @str: is pointer variable
+*/
+
+void puts2(char *str)
+
+{
+	int i, z, leng;
+
+	for (i = 0; (str[i] != '\0'); i++)
+	{
+
+	}
+	leng = i;
+	for (z = 0; z <= (leng - 1); z+=2)
+	{
+		_putchar(str[z]);
+	}
+	_putchar(10);
+}
