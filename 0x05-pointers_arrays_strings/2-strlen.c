80d867d3063425f286705689e949346152de0ae1
frankcamarena <fgustavocamarena@gmail.com>
2021-10-13 01:10:07 +0000

    function that returns the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* _strlen - function about ckeck for a digit
+* @s: is pointer variable
+* Return: i is correct
+*/
+
+int _strlen(char *s)
+
+{
+	int i;
+
+	for (i = 0; (s[i] != '\0'); ++i)
+	{
+
+	}
+		return (i);
+}
