07e59bc02522dabc62d3504ac354ed4b86ea0bf0
frankcamarena <fgustavocamarena@gmail.com>
2021-10-13 00:25:08 +0000

    swaps the values of two integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* swap_int - function about ckeck for a digit
+* @a: is pointer variable
+*/
+
+void swap_int(int *a, int *b)
+
+{
+	int t;
+
+	t = *a;
+	*a = *b;
+	*b = t;
+
+}
