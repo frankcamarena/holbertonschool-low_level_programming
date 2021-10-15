5799cdae4434200bde2cf26dc686b89f1d7eec56
frankcamarena <fgustavocamarena@gmail.com>
2021-10-15 14:22:22 +0000

    function that concatenates two strings with byte

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+* *_strncat - function about to print str
+* @dest: is a cadena
+* @src: es a cadena
+* Return: dest
+*/
+
+char *_strncat(char *dest, char *src, int n)
+
+{
+	int i;
+	int leng;
+
+	leng = 0;
+	while (dest[leng] != '\0')
+	{
+		++leng;
+	}
+
+	for (i = 0; i < n && (src[i] != '\0'); ++i)
+	{
+		dest[leng] = src[i];
+		++leng;
+	}
+	dest[leng] = '\0';
+	return (dest);
+}
