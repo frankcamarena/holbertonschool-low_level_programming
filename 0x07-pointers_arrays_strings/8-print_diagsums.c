aac884224dc6f159793f53ba4aecdedf4b78efdb
frankcamarena <fgustavocamarena@gmail.com>
2021-10-20 04:15:42 +0000

    Print sum diagonal

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* print_diagsums - funcion para sumar las diagonales de la matriz
+* @a: variable
+* @size: tamaño de la matriz
+*/
+
+void print_diagsums(int *a, int size)
+
+{
+	int x, suma1, suma2;
+
+	for (x = 0;  x < size; x++)
+	{
+		suma1 += *(a + ((x * size) + x));
+		suma2 += a[((x + 1) * (size - 1))];
+	}
+	printf("%d %d\n", suma1, suma2);
+}
